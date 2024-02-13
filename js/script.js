ee3bee21bd6cb75952a3a85dc9510f9f9c25ef3f
Sergeysu25 <sergeysu25@gmail.com>
2024-02-13 23:24:53 +0300

    all task without 6 and 1

---
+++ js/script.js
@@ -0,0 +1,66 @@
+"use strict"
+// Zadacha 1
+let x = 20;
+let y = 58;
+let z= 42;
+let q = x + y + z
+console.log(q)
+
+// Zadacha 2
+let secInMin = 60 ,
+    minInHour = 60 ,
+    hourInDay = 24 ,
+    dayInYear = 365;
+let myAge = 35;
+let myAgeInSeconds = myAge * dayInYear * hourInDay * minInHour * secInMin;
+console.log(myAgeInSeconds);
+
+// Zadacha 3
+let count = 42;
+count = String(count);
+console.log(typeof count);
+let userName = '42';
+userName = Number(userName);
+console.log(typeof userName);
+
+// Zadacha 4
+let a = 1;
+a = String(a);
+let b = 2;
+b = String(b);
+let c = 'белых медведей';
+console.log(a + b + ' ' + c);
+
+// Zadacha 5
+let d = 'доступ';
+let e = 'морпех';
+let f = 'наледь';
+let g = 'попрек';
+let h = 'рубило';
+let lengthWords = d.length + e.length + f.length + g.length + h.length
+console.log(lengthWords)
+
+// Zadacha 6?
+
+// Zadacha 7
+let username = prompt('Как тебя зовут?')
+let userAge = prompt('Сколько тебе лет?')
+console.log(username , userAge)
+
+
+// Zadacha 1
+
+let a1 = 4;
+let b1 = 3;
+[a1 , b1] = [b1 , a1];
+console.log(a1 , b1);
+
+// Zadacha 2
+let codeWord1 = "обернись";
+let codeWord2 = "неужели";
+let codeWord3 = "огурцы";
+let codeWord4 = "липкие";
+let codeWord5 = "?!";
+
+console.log(codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1])
+
