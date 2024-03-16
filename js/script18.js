86536da9c2b8527229f7bf550b33e3c1224e983c
Sergeysu25 <sergeysu25@gmail.com>
2024-03-16 23:02:12 +0300

    hw18 without 1.3

--- js/script18.js
+++ js/script18.js
@@ -91,34 +91,35 @@
 
 // function lomaemShokolad(a, b) {
 //     let c 
-//     if (a == Number, a > 0 , b == Number, b > 0) {
+//     if (typeof a == 'number' || a > 0 || typeof b == 'number' || b > 0) {
 //         return c = a - 1 + b - 1
 //     } else {
 //         return '0'
 //     }
 // }
-// alert('Ломаем шоколад ' + lomaemShokolad(30, 2) + ' раз!!!');
+// alert('Ломаем шоколад ' + lomaemShokolad(20, 21) + ' раз!!!');
 
 // Zadacha 1.3
-
-
 
 // Напишите программу для вычисления общей стоимости покупки телефонов.Вы будете продолжать покупать телефоны (подсказка: циклы!), пока у вас не закончатся деньги на банковском счете. Вы также будете покупать аксессуары для каждого из телефонов. После того как вы посчитаете сумму покупки, прибавьте налог, затем выведите на экран вычисленную сумму покупки, правильно отформатировав её. Наконец, сверьте сумму с балансом вашего банковского счета, чтобы понять, можете вы себе это позволить или нет. Вы должны настроить некоторые константы для «ставки налога», «цены телефона», «цены аксессуара», так же как и переменную для вашего «баланса банковского счета».
 // Вам следует определить функции для вычисления налога и для форматирования цены со знаком валюты и округлением до двух знаков после запятой.
 // Попробуйте включить ввод данных в вашу программу, например с помощью функции prompt(...) . Вы можете, например, запросить у пользователя баланс банковского счета. Развлекайтесь и будьте изобретательны
 
 
-let cash = Number(prompt('Введите сумму денег на счёте'))
-let nalog = 22
-let sumTelef = 450
-let sumAksesuarov = 70
+// let cash = Number(prompt('Введите сумму денег на счёте'))
+// let nalog = 22
+// let sumTelef = 450
+// let sumAksesuarov = 70
+// let sumNalogTelef = sumTelef * nalog / 100
+// let sumNalogAksessuarov = sumAksesuarov * nalog / 100
 
-function sumPokupki() {
-    if (cash < sumTelef * nalog / 100) {
-        console.log('Не хватает денег на счете!!!');
-    } else if (cash <= sumTelef * nalog / 100) { 
-        return sumPokupki = sumTelef * nalog / 100 + sumAksesuarov * nalog / 100
-    }
+// function sumPokupki() {
 
-}
+//     if (cash < sumTelef + sumNalogTelef) {
+//         alert('Не хватает денег на счете!!!');
+//     } else if (cash <= sumTelef + sumNalogTelef + sumAksesuarov + sumNalogAksessuarov) {
+//         alert(`Сумма покупки -`+ `${sumTelef + sumNalogTelef + sumAksesuarov + sumNalogAksessuarov}`);
+//     }
 
+// }
+// sumPokupki()
